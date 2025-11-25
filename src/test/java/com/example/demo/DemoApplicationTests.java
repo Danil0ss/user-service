7e967efdc2a46c7ba0f6f760b96b0b5569a461d2
Danil0ss <2005prudnikov@gmail.com>
2025-11-25 22:17:24 +0300

    Revert "CI"
    This reverts commit 1ad58c8b6af32c4f1fc2f2d92fee21e11bef4a84.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,13 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.cache.type=none",
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
-})
+@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
