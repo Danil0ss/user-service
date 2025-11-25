1019968b8ac1e0f34c53d7b09eead0bcfa446ccf
Danil0ss <2005prudnikov@gmail.com>
2025-11-25 19:44:18 +0300

    CI

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// ====================================================================================================
+// Изолируем Unit Test от ВСЕХ зависимостей DB/JPA/Liquibase/Cache
+// ====================================================================================================
+
 @SpringBootTest(properties = {
 		"spring.liquibase.enabled=false",
-
-		"spring.datasource.url=jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+		"spring.jpa.hibernate.ddl-auto=none",
 
 		"spring.autoconfigure.exclude=" +
 				"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
