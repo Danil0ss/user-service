bdf5f2adf0e5fdb00c334433e6a62adb15bcfed5
Danil0ss <2005prudnikov@gmail.com>
2025-11-25 20:54:54 +0300

    CI

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,33 +2,28 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration; // НОВЫЙ ИМПОРТ
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootTest(
 		properties = {
-				// КЛЮЧЕВОЕ ИЗМЕНЕНИЕ: РАЗРЕШАЕМ ПЕРЕОПРЕДЕЛЕНИЕ БИНОВ (для Unit Test это допустимо)
-				"spring.main.allow-bean-definition-overriding=true", // <-- ЭТО РЕШАЕТ ПРОБЛЕМУ
 
-				// Отключение Liquibase и кэша
+				"spring.main.allow-bean-definition-overriding=true",
 				"spring.liquibase.enabled=false",
 				"spring.cache.type=none",
-				"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1" // Используем H2-заглушку
+				"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"
 		}
 )
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// Тест проходит, если IoC-контейнер стартует.
 	}
 
-	// Вложенный @Configuration для переопределения CacheManager
 	@Configuration
 	static class TestApplicationConfiguration {
 
-		// Переопределяем CacheManager для Unit Test.
-		// @Bean cacheManager теперь переопределит RedisConfig.cacheManager
+
 		@org.springframework.context.annotation.Bean
 		public org.springframework.cache.CacheManager cacheManager() {
 			return new org.springframework.cache.support.NoOpCacheManager();
