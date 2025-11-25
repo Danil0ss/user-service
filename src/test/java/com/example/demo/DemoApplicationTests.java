56ebb33c5de4c9abcba1188521e45835f6ced90a
Danil0ss <2005prudnikov@gmail.com>
2025-11-25 19:39:02 +0300

    CI

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-		"spring.cache.type=none",
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+		"spring.liquibase.enabled=false",
+
+		"spring.datasource.url=jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+
+		"spring.autoconfigure.exclude=" +
+				"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 }
